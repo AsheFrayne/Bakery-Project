3a8dc109e742649b9de8a6d216666a10bb5ca8fe
NekoIchigo <reydanjohnbelen@gmail.com>
2022-02-01 02:57:14 +0800

    editcommentdelte

--- includes/editDeleteOffers.inc.php
+++ includes/editDeleteOffers.inc.php
@@ -24,14 +24,61 @@ if (isset($_POST["submit"])) {
   $productsName = $_POST['productsName'];
   $productsID = $_POST['productsID'];
 
+  $sql = "UPDATE products SET productsName = '$productsName' WHERE productsID='$productsID';";
+  mysqli_query($conn, $sql);
+
+  $check = "SELECT productsName FROM products WHERE productsID = '$productsID';";
+  $result = mysqli_query($conn, $check);
+  $products = mysqli_fetch_assoc($result);
+  if ($products['productsName'] == $filename) {
+    header('location: ../offers.php?modal=1&productID='.$productsID.'&productsName=success');
+  }else{
+    header('location: ../offers.php?modal=1&productID='.$productsID.'&productsName=failed');
+  }
+
 }elseif (isset($_POST["description"])) {
-  $productsName = $_POST['productsName'];
+  $productsDescription = $_POST['productsDescription'];
   $productsID = $_POST['productsID'];
 
-}elseif (isset($_POST["price"])) {
-  $productsName = $_POST['productsName'];
-  $productsID = $_POST['productsID'];
+  $sql = "UPDATE products SET productsDescription = '$productsDescription' WHERE productsID='$productsID';";
+  mysqli_query($conn, $sql);
+
+  $check = "SELECT productsDescription FROM products WHERE productsID = '$productsID';";
+  $result = mysqli_query($conn, $check);
+  $products = mysqli_fetch_assoc($result);
+  if ($products['productsDescription'] == $filename) {
+    header('location: ../offers.php?modal=1&productID='.$productsID.'&productsDescription=success');
+  }else{
+    header('location: ../offers.php?modal=1&productID='.$productsID.'&productsDescription=failed');
+  }
 
 }elseif (isset($_POST["price"])) {
-  // code...
+  $productsPrice = $_POST['productsPrice'];
+  $productsID = $_POST['productsID'];
+
+  $sql = "UPDATE products SET productsPrice = '$productsPrice' WHERE productsID='$productsID';";
+  mysqli_query($conn, $sql);
+
+  $check = "SELECT productsPrice FROM products WHERE productsID = '$productsID';";
+  $result = mysqli_query($conn, $check);
+  $products = mysqli_fetch_assoc($result);
+  if ($products['productsPrice'] == $filename) {
+    header('location: ../offers.php?modal=1&productID='.$productsID.'&productsDescription=success');
+  }else{
+    header('location: ../offers.php?modal=1&productID='.$productsID.'&productsDescription=failed');
+  }
+
+}elseif (isset($_POST["delete"])) {
+  $productsID = $_POST['productsID'];
+
+  $sql = "DELETE FROM products WHERE productsID='$productsID';";
+  mysqli_query($conn, $sql);
+
+  $check = "SELECT * FROM products WHERE productsID = '$productsID';";
+  $result = mysqli_query($conn, $check);
+  if (!$products = mysqli_fetch_assoc($result);) {
+    header('location: ../offers.php?modal=1&productID='.$productsID.'&delete=success');
+  }else{
+    header('location: ../offers.php?modal=1&productID='.$productsID.'&delete=failed');
+  }
 }
