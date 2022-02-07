dc89f741c94f319ba640c3aec1c56688a618d5d4
NekoIchigo <reydanjohnbelen@gmail.com>
2022-02-07 20:52:35 +0800

    fix bug in login

--- login.php
+++ login.php
@@ -1,6 +1,10 @@
+<?php
+  if (isset($_SESSION["usersID"])) {
+    header('location: index.php');
+  }
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
